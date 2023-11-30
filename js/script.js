b7caea1c635405b72d42cca02efc4b370c860428
birthday-mode <152472883+birthday-mode@users.noreply.github.com>
2023-11-30 01:35:01 -0500

    innit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('#rulesToggle').click(function () {
+    $('#rules').css('display', 'flex');
+    $('#leaderboard').css('display', 'none');
+});
+
+ $('#leaderboardToggle').click(function () {
+    $('#leaderboard').css('display', 'flex');
+    $('#rules').css('display', 'none');
+});
